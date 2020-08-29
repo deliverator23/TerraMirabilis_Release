177aa2bbda79b1b804a8c86807a2ef4865e42630
deliverator23 <iain.millar@gmail.com>
2020-08-29 09:31:32 +0100

    Re-instate Fountain of Youth ability fix.

--- NaturalWondersMod/Core/Natural Wonders/DLC/TM_FountainOfYouth.sql
+++ NaturalWondersMod/Core/Natural Wonders/DLC/TM_FountainOfYouth.sql
@@ -27,6 +27,14 @@ WHERE	FeatureType = 'FEATURE_FOUNTAIN_OF_YOUTH';
 	WHERE ModifierId IN ('FOUNTAIN_OF_YOUTH_UNITS_GRANT_ABILITY')
 	AND EXISTS (SELECT * FROM TM_UserSettings WHERE Setting = 'NW_EFFECTS' AND Value = 1);
 
+	DELETE FROM UnitAbilityModifiers
+	WHERE UnitAbilityType IN ('ABILITY_WATER_OF_LIFE')
+	AND EXISTS (SELECT * FROM TM_UserSettings WHERE Setting = 'NW_EFFECTS' AND Value = 1);
+
+	DELETE FROM UnitAbilities
+	WHERE UnitAbilityType IN ('ABILITY_WATER_OF_LIFE')
+	AND EXISTS (SELECT * FROM TM_UserSettings WHERE Setting = 'NW_EFFECTS' AND Value = 1);
+
 	UPDATE	Features
 	SET		Description = 'LOC_TM_FEATURE_FOUNTAIN_OF_YOUTH_ORIGINAL_EFFECT_DESCRIPTION'
 	WHERE	FeatureType = 'FEATURE_FOUNTAIN_OF_YOUTH'
@@ -35,22 +43,22 @@ WHERE	FeatureType = 'FEATURE_FOUNTAIN_OF_YOUTH';
 -----------------------------------------------
 -- Types
 -----------------------------------------------
-	
+
 INSERT INTO Types
 		(Type,										Kind			)
 VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'KIND_ABILITY'	);
 
 -----------------------------------------------
 -- Tags
------------------------------------------------	
-	
+-----------------------------------------------
+
 INSERT INTO Tags
 		(Tag,										Vocabulary		)
 VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'ABILITY_CLASS'	);
 
 -----------------------------------------------
 -- TypeTags
------------------------------------------------		
+-----------------------------------------------
 
 INSERT INTO TypeTags
 		(Type,										Tag							)
@@ -62,12 +70,12 @@ VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'CLASS_ALL_COMBAT_UNITS'	);
 
 INSERT INTO UnitAbilities
 		(UnitAbilityType,							Name,												Description,											Inactive,	ShowFloatTextWhenEarned,	Permanent	)
-VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'LOC_ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH_NAME',	'LOC_ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH_DESCRIPTION',	0,			1,							1			);
+VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'LOC_ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH_NAME',	'LOC_ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH_DESCRIPTION',	1,			0,							1			);
 
 -----------------------------------------------
 -- UnitAbilityModifiers
 -----------------------------------------------
-		
+
 INSERT INTO UnitAbilityModifiers
 		(UnitAbilityType,							ModifierId												)
 VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'MODIFIER_TM_FEATURE_FOUNTAIN_OF_YOUTH_UNIT_HEALING'	);
@@ -79,7 +87,7 @@ VALUES	('ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH',	'MODIFIER_TM_FEATURE_FOUNTAIN_OF
 INSERT INTO Modifiers (ModifierId, ModifierType, OwnerRequirementSetId, SubjectRequirementSetId)
 SELECT	'MODIFIER_TM_FEATURE_FOUNTAIN_OF_YOUTH_ATTACH_PLAYERS',
 		'MODIFIER_ALL_PLAYERS_ATTACH_MODIFIER',
-		'REQSET_TM_MAP_HAS_FEATURE_FOUNTAIN_OF_YOUTH',	
+		'REQSET_TM_MAP_HAS_FEATURE_FOUNTAIN_OF_YOUTH',
 		'REQSET_TM_PLAYER_HAS_FEATURE_FOUNTAIN_OF_YOUTH'
 WHERE EXISTS (SELECT * FROM Features WHERE FeatureType = 'FEATURE_FOUNTAIN_OF_YOUTH');
 
@@ -101,5 +109,3 @@ WHERE EXISTS (SELECT * FROM Features WHERE FeatureType = 'FEATURE_FOUNTAIN_OF_YO
 INSERT INTO ModifierArguments
 		(ModifierId,											Name,			Value									)
 VALUES	('MODIFIER_TM_FEATURE_FOUNTAIN_OF_YOUTH_GRANT_ABILITY',	'AbilityType',	'ABILITY_TM_FEATURE_FOUNTAIN_OF_YOUTH'	);
-
-
